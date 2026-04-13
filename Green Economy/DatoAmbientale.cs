5e6ff160a4eedeac264a0a32e26338689207b467
Ivan <12999537@itisrossi.vi.it>
2026-04-13 22:29:28 +0200

    Aggiungere i file di progetto.

---
+++ Green Economy/DatoAmbientale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Economy
+{
+    public class DatoAmbientale
+    {
+        public string Provincia { get; set; }
+        public DateTime DataOra { get; set; }
+        public double Temperatura { get; set; }
+        public int Inquinamento { get; set; }
+        public int Umidita { get; set; }
+        public double Vento { get; set; }
+    }
+}
